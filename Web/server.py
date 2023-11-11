c9543347444907951dec83716b0f0970eb44255f
Austin Salmon <ajsalmon0831@gmail.com>
2023-11-11 10:04:01 -0600

    initial flask attempt

---
+++ Web/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return render_template('index.html', prediction='YESSSSS')
