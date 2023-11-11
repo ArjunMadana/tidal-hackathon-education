681260112c33f8b601f0a621847694e6bed2f978
Austin Salmon <ajsalmon0831@gmail.com>
2023-11-11 10:31:36 -0600

    interface flask server with html

--- Web/app.py
+++ Web/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', prediction='HOME')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict')
 def predict():
     return render_template('index.html', prediction='YESSSSS')
